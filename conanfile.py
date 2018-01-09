f0349df7a9b95d8f14296db531d939f3d101c52e
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-01-09 18:35:11 +0300

    Update

--- conanfile.py
+++ conanfile.py
@@ -10,25 +10,16 @@ class GoogletestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
     exports_sources = "src/*"
+    no_copy_source = True
     build_policy = "missing"
 	
     def build(self):
         cmake = CMake(self)
-        if (
-                self.settings.os == "Windows" and self.settings.compiler == "Visual Studio" and
-                self.settings.compiler.toolset is not None and self.settings.compiler.toolset != "None"
-           ):
-            self.output.info("MSVC toolset: %s" % self.settings.compiler.toolset)
-            self.run("cmake %s/src -T %s %s %s %s" % (\
-                self.source_folder, \
-                self.settings.compiler.toolset, \
-                cmake.command_line, \
-                "-Dgtest_force_shared_crt=ON", \
-                "-DCMAKE_CXX_STANDART=11"))
-        else:
-            self.run("cmake %s/src %s %s" % (self.source_folder, cmake.command_line, "-DCMAKE_CXX_STANDART=11"))
-
-        self.run("cmake --build . %s" % cmake.build_config)
+        cmake.definitions["CMAKE_CXX_STANDART"] = "11"
+        if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
+            cmake.definitions["gtest_force_shared_crt:BOOL"] = "ON"
+        cmake.configure(source_folder="src")
+        cmake.build()
 
     def package(self):
         # headers
@@ -36,13 +27,18 @@ class GoogletestConan(ConanFile):
         self.copy("*.h", dst="include", src="src/googlemock/include")
         # libraries
         self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.pdb", dst="bin", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*gtest.pdb", dst="bin", keep_path=False)
+        self.copy("*gmock.pdb", dst="bin", keep_path=False)
+        self.copy("*gtest_main.pdb", dst="bin", keep_path=False)
+        self.copy("*gmock_main.pdb", dst="bin", keep_path=False)
+        self.copy("*gtestd.pdb", dst="bin", keep_path=False)
+        self.copy("*gmockd.pdb", dst="bin", keep_path=False)
+        self.copy("*gtest_maind.pdb", dst="bin", keep_path=False)
+        self.copy("*gmock_maind.pdb", dst="bin", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["gmock_main"]
+        self.cpp_info.libs = ["gmock_main"] if self.settings.build_type == "Release" else ["gmock_maind"]
         self.cpp_info.defines = ["GTEST_LANG_CXX11"]
 
         if self.settings.os == "Linux":
