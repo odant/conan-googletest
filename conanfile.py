bb18b90629a11dda6ef1580345e159a7dfa11158
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-04-13 20:15:08 +0300

    Bump version 1.9.0-b1

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake
 
 class GoogletestConan(ConanFile):
     name = "googletest"
-    version = "1.8.0"
+    version = "1.9.0-b1"
     license = "BSD 3-clauses https://github.com/google/googletest/blob/master/googletest/LICENSE"
     description = "Google's C++ test framework"
     url = "https://github.com/odant/conan-googletest"
