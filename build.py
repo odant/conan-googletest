2fe1cd4e782883bddaa67a7546eb1bb5751d59f2
d0nc1h0t <udincev@yandex.ru>
2019-11-15 11:00:40 +0300

    Build for VC 2017, 2019

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ from conan.packager import ConanMultiPackager
 # Common settings
 username = "odant" if "CONAN_USERNAME" not in os.environ else None
 # Windows settings
-visual_versions = ["15"] if "CONAN_VISUAL_VERSIONS" not in os.environ else None
+visual_versions = ["15", "16"] if "CONAN_VISUAL_VERSIONS" not in os.environ else None
 visual_runtimes = ["MD", "MDd", "MT", "MTd"] if "CONAN_VISUAL_RUNTIMES" not in os.environ else None
 
 
