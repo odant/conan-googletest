625c986ca47b7d8f1a24c487bbf22f36c0d82425
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2020-04-22 03:23:13 +0300

    Update CI

--- build.py
+++ build.py
@@ -1,42 +1,23 @@
-import platform, os
-from copy import deepcopy
+# Build Conan package
+# Dmitriy Vetutnev, ODANT, 2020
+
+
 from conan.packager import ConanMultiPackager
+import os
 
 
-# Common settings
-username = "odant" if "CONAN_USERNAME" not in os.environ else None
-# Windows settings
-visual_versions = ["15", "16"] if "CONAN_VISUAL_VERSIONS" not in os.environ else None
-visual_runtimes = ["MD", "MDd", "MT", "MTd"] if "CONAN_VISUAL_RUNTIMES" not in os.environ else None
-
-
-def filter_libcxx(builds):
-    result = []
-    for settings, options, env_vars, build_requires, reference in builds:
-        if settings["compiler.libcxx"] == "libstdc++11":
-            result.append([settings, options, env_vars, build_requires, reference])
-    return result
+env_pure_c = os.getenv("CONAN_PURE_C", True)
+pure_c = True if str(env_pure_c).lower() != "false" else False
 
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(
-        username=username,
-        visual_versions=visual_versions,
-        visual_runtimes=visual_runtimes,
         exclude_vcvars_precommand=True
     )
-    builder.add_common_builds(pure_c=False)
-    # Adjusting build configurations
-    builds = builder.items
-    if platform.system() == "Linux":
-        builds = filter_libcxx(builds)
-    # Replace build configurations
-    builder.items = []
-    for settings, options, env_vars, build_requires, _ in builds:
-        builder.add(
-            settings=settings,
-            options=options,
-            env_vars=env_vars,
-            build_requires=build_requires
-        )
+    builder.add_common_builds(
+        pure_c=pure_c
+    )
+    builder.remove_build_if(
+        lambda build: build.settings.get("compiler.libcxx") == "libstdc++"
+    )
     builder.run()
