c55fd5094107647359ddf42d8827cd169cd25b75
d0nc1h0t <udincev@yandex.ru>
2025-02-18 14:01:04 +0300

    Version1.16.0+0

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ from conans import ConanFile, CMake, tools
 
 class GoogletestConan(ConanFile):
     name = "googletest"
-    version = "1.15.2+0"
+    version = "1.16.0+0"
     license = "BSD 3-clauses https://github.com/google/googletest/blob/master/googletest/LICENSE"
     description = "Google's C++ test framework"
     url = "https://github.com/odant/conan-googletest"
